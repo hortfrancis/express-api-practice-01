f3c277700b5bb4d16ac1a9f0c4d8392dc6dd8570
hortfrancis <ajhf@hotmail.co.uk>
2024-01-19 14:55:23 +0000

    Add OpenAI API calls and associated routes

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
 import express from 'express';
+import { listModels, callCompletionAPI, callChatAPI } from './lib/openai.js';
+
 const app = express();
 
 app.get(
-    '/',
-    (req, res) => {
-        res.send("Hello!")
+    '/list-models',
+    // Display a list of available models from the OpenAI API
+    async (req, res) => {
+        res.send(await listModels());
+    }
+)
+
+app.get(
+    '/chat-completion',
+    // Return a default chat completion from the OpenAI API
+    // Model: GPT-3.5 
+    async (req, res) => {
+        res.send(await callChatAPI())
+    }
+)
+
+app.get(
+    '/completion',
+    // Return a text default completion from the OpenAI API
+    async (req, res) => {
+        res.send(await callCompletionAPI());
     }
 );
 
